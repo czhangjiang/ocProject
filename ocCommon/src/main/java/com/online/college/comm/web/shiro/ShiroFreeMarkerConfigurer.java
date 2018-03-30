6c11291d6e8c570a94dec2124731975554d7247f
01195957 <zhang.weitao@sf-express.com>
2018-03-30 18:25:50 +0800

    初始化程序

---
+++ ocCommon/src/main/java/com/online/college/comm/web/shiro/ShiroFreeMarkerConfigurer.java
@@ -0,0 +1,16 @@
+package com.online.college.comm.web.shiro;
+
+import com.online.college.comm.web.shiro.freemarker.ShiroTags;
+import freemarker.template.TemplateException;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import java.io.IOException;
+
+public class ShiroFreeMarkerConfigurer extends FreeMarkerConfigurer {
+
+    @Override
+    public void afterPropertiesSet() throws IOException, TemplateException {
+        super.afterPropertiesSet();
+        this.getConfiguration().setSharedVariable("shiro", new ShiroTags());
+    }
+}
